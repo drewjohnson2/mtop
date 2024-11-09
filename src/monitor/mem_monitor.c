3c377f4eaa38130a2ff0714fe2d7193457173fe0
Drew <djohnson104@uco.edu>
2024-11-09 10:34:19 -0600

    i don't even remember the point of that stupid code. Maybe at one point those were the only two things I wanted from that file? Idk dude

--- src/monitor/mem_monitor.c
+++ src/monitor/mem_monitor.c
@@ -3,7 +3,6 @@
 
 #include "../include/mem_monitor.h"
 
-static inline int _str_starts_with(char *str1, char *str2, size_t len);
 static void _parse_stat(MEMORY_STATS *stat, char *buffer);
 
 MEMORY_STATS * fetch_memory_stats(Arena *arena)
@@ -19,10 +18,6 @@ MEMORY_STATS * fetch_memory_stats(Arena *arena)
 
 	while (fgets(buffer, sizeof(buffer), f))
 	{
-		if (_str_starts_with(buffer, "MemTotal:", 9) != 0 && 
-	  		_str_starts_with(buffer, "MemFree:", 8) != 0)
-			break;
-
 		_parse_stat(stat, buffer);
 	}
 
@@ -39,15 +34,4 @@ static void _parse_stat(MEMORY_STATS *stat, char *buffer)
 	else if (sscanf(buffer, "Cached: %llu kB\n", &stat->cachedMem) == 1) return;
 	else if (sscanf(buffer, "Shmem: %llu kB\n", &stat->shared) == 1) return;
 	else if (sscanf(buffer, "SReclaimable: %llu kB\n", &stat->sReclaimable) == 1) return;
-}
-
-static inline int _str_starts_with(char *str1, char *str2, size_t len)
-{
-	char hold[len + 1];
-
-	for (size_t i = 0; i < len; i++) hold[i] = str1[i];
-
-	hold[len] = '\0';
-
-	return strcmp(hold, str2);
 }
