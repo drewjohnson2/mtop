88b90c1b80a9a28c65f87372784eb1c4419da205
Drew <djohnson104@uco.edu>
2024-11-30 01:07:06 -0600

    I added this change because the more I thought about how integer types are defined between machines started to freak me out even though this is an issue I'll probably never encounter. Either way, better to be safe I suppose.

--- src/include/mt_type_defs.h
+++ src/include/mt_type_defs.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 
 typedef uint8_t u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long u64;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
 #endif
