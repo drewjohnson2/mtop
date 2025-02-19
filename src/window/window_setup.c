972c55c7a6405e4b459988da7ee5fc01bbd3453e
Drew <djohnson104@uco.edu>
2025-02-18 21:09:36 -0600

    Trying this on another machine

--- src/window/window_setup.c
+++ src/window/window_setup.c
@@ -113,8 +113,16 @@ void init_window_dimens(DisplayItems *di)
     optWin->windowX = (container->wWidth / 2) - (container->wWidth / 8);
     optWin->windowY = (container->wHeight / 2) - (container->wHeight / 8);
 
-    if (optWin->wHeight < 16) optWin->wHeight = 17;
-    if (optWin->wWidth < 50) optWin->wWidth = 55;
+    if (optWin->wHeight < 16)
+    {
+	optWin->wHeight = 17;
+	optWin->windowY -= optWin->wHeight;
+    }
+    if (optWin->wWidth < 50)
+    {
+	optWin->wWidth = 55;
+	optWin->windowX -= optWin->wWidth;
+    }
 }
 
 void init_windows(DisplayItems *di) 
